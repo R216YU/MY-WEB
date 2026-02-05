39ca039f355f62dae354dabe89c3733180356fbe
R216YU <r216yu@gmail.com>
2026-02-06 01:06:39 +0900

    update: error, notfoundページを追加

---
+++ src/app/error.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+const Error = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Error</h1>
+      <p className="text-lg">An error occurred while loading the page.</p>
+    </div>
+  );
+};
+
+export default Error;
